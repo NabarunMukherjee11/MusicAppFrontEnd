956fddcf8743be7848308ea8c3ca52aea628fac2
NabarunMukherjee11 <nabarunm21@gmail.com>
2024-05-13 19:11:10 +0530

    Minor changes

--- spe-frontend/vite.config.js
+++ spe-frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    watch: {
+      usePolling: true, // Enable polling for file watching
+    },
+    host: true, // Allow Vite to use the default host (typically localhost)
+    strictPort: true, // Ensure that the specified port is used
+    port: 3000, // Set the port to 8080
+  },
+});
